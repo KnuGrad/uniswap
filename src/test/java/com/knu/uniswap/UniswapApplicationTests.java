6ab4d00d3c8182e0540eb2b3dbc2273727a8ae66
JunHyeongChoi <ajfeh22@gmail.com>
2024-04-17 19:49:50 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/com/knu/uniswap/UniswapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knu.uniswap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniswapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
